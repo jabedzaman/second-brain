d900ddf82c867f83f27ff162b491d5b0ce0dc612
jabedzaman <hi@jabed.dev>
2025-10-11 02:31:17 +0530

    feat: scaffold nextjs frontend

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+
+const urbanist = Urbanist({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Brain",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${urbanist.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
